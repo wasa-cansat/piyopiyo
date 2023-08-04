bb27db6f239dee027217670b64a73353770fe3ce
gamingyoda <116149256+gamingyoda@users.noreply.github.com>
2023-08-05 03:43:14 +0900

    Update 9DoF_check.ino

--- 9DoF_check.ino
+++ 9DoF_check.ino
@@ -1,1 +1,5 @@
-
+//参考
+//https://github.com/adafruit/Adafruit_BNO055
+//http://shinshu-makers.net/shinshu_makers/2023/01/21/%E3%80%90sta22%E3%80%91%E5%A7%BF%E5%8B%A2%E3%82%BB%E3%83%B3%E3%82%B5%E3%81%AB%E7%A7%8B%E6%9C%88%E3%81%AEbno055%E3%82%92%E8%A9%A6%E3%81%99%EF%BC%9Cyaw%E8%A7%92%E7%B2%BE%E5%BA%A6%EF%BC%91%EF%BD%9E/
+//https://gist.github.com/dj1711572002/a95b6944ce5ccb60faecd0a3edfcf9df
+//https://gist.github.com/dj1711572002/82216217b96c8abc2d62b5c6d122bf87
