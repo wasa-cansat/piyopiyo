56a922ac218165eb38763355bbceb49236ec83d3
rogue1starwars <87596452+rogue1starwars@users.noreply.github.com>
2023-08-05 15:26:53 +0900

    フォルダに入れました。

---
+++ GPS_SDcard_check/GPS_SDcard_check.ino
@@ -0,0 +1,94 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <SD.h>
+
+File myFile;
+TinyGPSPlus gps;
+double destLatitude = 35.9064485;    // 目的地の緯度
+double destLongitude = 139.6238548;   // 目的地の経度
+
+int RX_PIN = 1;
+int TX_PIN = 0;
+
+SoftwareSerial gpsSerial(RX_PIN, TX_PIN);
+
+struct LocationData {
+  double latitude;
+  double longitude;
+  double bearing;
+  double distance;
+};
+
+void sd_setup(){
+  pinMode(10, OUTPUT);
+  while(!SD.begin(10)){
+    delay(1000);
+  }
+}
+
+void sd_GPSwrite(double latitude, double longitude, double bearing, double distance){
+  myFile = SD.open("log.txt", FILE_WRITE);
+  if (myFile){
+    myFile.print("time: ");
+    unsigned long time = millis();
+    myFile.println(String(time/1000));
+    if(latitude != 0.0){
+      myFile.print(" Latitude: ");
+      myFile.print(String(latitude,9));
+      Serial.println(latitude, 9);
+      myFile.print(" longitude: ");
+      myFile.print(String(longitude,9));
+      Serial.println(longitude, 9);
+      myFile.print(" bearing: ");
+      myFile.print(String(bearing,9));
+      Serial.println(bearing, 9);
+      myFile.print(" distance: ");
+      myFile.println(String(distance,9));
+      Serial.println(distance, 9);
+    }
+  }
+  myFile.close();
+}
+
+void getGPSData() {
+  static LocationData data = {0.0, 0.0, 0.0, 0.0}; 
+
+  while (gpsSerial.available() > 0) {
+    
+    char c = gpsSerial.read();
+    gps.encode(c);
+    
+    if (gps.location.isUpdated()) {
+
+    data.latitude = gps.location.lat();
+    data.longitude = gps.location.lng();
+
+    data.bearing = TinyGPSPlus::courseTo(
+        data.latitude, data.longitude,
+        destLatitude, destLongitude
+    );
+
+    data.distance = TinyGPSPlus::distanceBetween(
+        data.latitude, data.longitude,
+        destLatitude, destLongitude
+    );
+
+    sd_GPSwrite(data.longitude, data.latitude, data.bearing, data.distance);
+  }
+}
+}
+
+void setup() {
+  Serial.begin(115200);
+  gpsSerial.begin(9600);
+  pinMode(RX_PIN, INPUT);
+  pinMode(TX_PIN, OUTPUT);
+  sd_setup();
+}
+
+void loop() {
+  getGPSData();
+
+  //delay(1000); // Check GPS data every 1 second
+  //}
+}
---
+++ bno005_test/bno005_test.ino
@@ -0,0 +1,118 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+double maxx;
+double maxy;
+
+/* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
+   which provides a common 'type' for sensor data and some helper functions.
+
+   To use this driver you will also need to download the Adafruit_Sensor
+   library and include it in your libraries folder.
+
+   You should also assign a unique ID to this sensor for use with
+   the Adafruit Sensor API so that you can identify this particular
+   sensor in any data logs, etc.  To assign a unique ID, simply
+   provide an appropriate value in the constructor below (12345
+   is used by default in this example).
+
+   Connections
+   ===========
+   Connect SCL to analog 5
+   Connect SDA to analog 4
+   Connect VDD to 3.3-5V DC
+   Connect GROUND to common ground
+
+   History
+   =======
+   2015/MAR/03  - First release (KTOWN)
+*/
+
+/* Set the delay between fresh samples */
+uint16_t BNO055_SAMPLERATE_DELAY_MS = 100;
+
+// Check I2C device address and correct line below (by default address is 0x29 or 0x28)
+//                                   id, address
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire);
+
+void setup(void)
+{
+  Serial.begin(115200);
+
+  while (!Serial) delay(10);  // wait for serial port to open!
+
+  Serial.println("Orientation Sensor Test"); Serial.println("");
+
+  /* Initialise the sensor */
+  if (!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1);
+  }
+
+  delay(1000);
+}
+
+void loop(void)
+{
+
+
+  double error = 0;
+  while(1){
+      sensors_event_t orientationData, magnetometerData;
+
+    bno.getEvent(&orientationData);
+    bno.getEvent(&magnetometerData, Adafruit_BNO055::VECTOR_MAGNETOMETER);
+  
+  //
+  //printEvent(&orientationData);
+  //  printEvent(&angVelocityData);
+  //  printEvent(&linearAccelData);
+  
+    double x = orientationData.orientation.x;
+    double y = orientationData.orientation.y;
+    double z = orientationData.orientation.z;
+    double mag_x = magnetometerData.magnetic.x;
+    double mag_y = magnetometerData.magnetic.y;
+    double mag_z = magnetometerData.magnetic.z;
+    if(abs(mag_y + 0.06) < 0.0001 || abs(mag_y + 8.06) < 0.0001 || (mag_x + mag_y + mag_z) > 2000){
+        Serial.println("not valid");
+    }
+    else{
+        Serial.print("mag_x = ");
+        Serial.println(atan2(mag_y, mag_x)*180/PI + 180);
+        error = atan2(mag_y, mag_x)*180/PI + 180 - x;
+        Serial.print("error: ");
+        Serial.println(error);
+    }
+    Serial.print("x = ");
+    Serial.println((x + error)<360 ? x + error: x + error - 360.0);
+    delay(100);
+  }
+
+//  printEvent(&magnetometerData);
+//  printEvent(&accelerometerData);
+//  printEvent(&gravityData);
+//
+//  int8_t boardTemp = bno.getTemp();
+//  Serial.println();
+//  Serial.print(F("temperature: "));
+//  Serial.println(boardTemp);
+//
+//  uint8_t system, gyro, accel, mag = 0;
+//  bno.getCalibration(&system, &gyro, &accel, &mag);
+//  Serial.println();
+//  Serial.print("Calibration: Sys=");
+//  Serial.print(system);
+//  Serial.print(" Gyro=");
+//  Serial.print(gyro);
+//  Serial.print(" Accel=");
+//  Serial.print(accel);
+//  Serial.print(" Mag=");
+//  Serial.println(mag);
+//
+//  Serial.println("--");
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+}
