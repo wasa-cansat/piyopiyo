ece0a78a050fa8f70cc022368bcf343b0d0e2555
rogue1starwars <87596452+rogue1starwars@users.noreply.github.com>
2023-08-10 18:49:22 +0900

    main changed

--- main_gps_bno_sd_camera_bmp/main_gps_bno_sd_camera_bmp.ino
+++ main_gps_bno_sd_camera_bmp/main_gps_bno_sd_camera_bmp.ino
@@ -7,6 +7,8 @@
 #include <SD.h>
 #include "motor.h"
 #include "HUSKYLENS.h"
+#include <Adafruit_BMP280.h>
+
 
 #include <Servo.h>
 
@@ -22,10 +24,11 @@ void findObject();
 void findObjectSetup();
 bool cam_sw = false;
 
+//35.704978,139.713760
 //GPS関連
 TinyGPSPlus gps;
-double destLatitude = 35.705287;    // 目的地の緯度
-double destLongitude = 139.714019;  // 目的地の経度
+double destLatitude = 35.704978;    // 目的地の緯度
+double destLongitude = 139.713760;  // 目的地の経度
 
 int RX_PIN = 0;
 int TX_PIN = 1;
@@ -66,6 +69,70 @@ void sd_setup();
 const int chipSelect = 10;
 void sd_GPSwrite(double latitude, double longitude, double bearing, double distance, double angle);
 
+//落下検知
+void sd_write(double height, double accel, double stat){
+  if (myFile){
+
+      myFile.print("height");
+      myFile.println(height);
+      myFile.print("accel");
+      myFile.println(accel);
+      myFile.print("status");
+      myFile.println(stat);
+    }
+  else{
+    Serial.println("cannot write to file");
+  }
+}
+
+
+// 落下距離(m)の閾値[30.0 ±0.6]
+const float fallDistance = 1.0;
+// 自由落下時の加速度(m/s2)の閾値[0.5]
+const float fallAccelThreshold = 9.68;
+// 着地後静止時の加速度変化(m/s2)の誤差範囲(理論値は0)[0.1]
+const float landedAccelThreshold = 0.1;
+// 着地後静止時の気圧変化(Pa)の誤差範囲(理論値は0）[30]
+const float landedPressureThreshold = 30;
+// 着地後静止時の大まかな検出待機時間(自然数sec)[15]
+const unsigned long landedIdleDuration = 1;
+// 落下開始から強制分離までの時間(sec)[90]
+const unsigned long ForcequitDuration = 5;
+
+
+//BME280
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+Adafruit_BMP280 bmp; // I2C
+
+//MNO055
+// Check I2C device address and correct line below (by default address is 0x29 or 0x28)
+//                                   id, address
+
+// 地表の気圧(kPa)
+float basePressure = 101.325;
+// 上昇高度の最高値を記録する変数
+float highestAltitude = 0.0;
+// ステータス定義
+enum State {
+  CHECK_FALLING,
+  CHECK_LANDED,
+  EXIT,
+  COMPLETE
+};
+//最初のステータス
+State state = CHECK_FALLING;
+//　落下開始時刻を記録する変数
+unsigned long fallStartTime;
+// 前回の加速度を記録する変数
+float lastAccel;
+// 前回の気圧を記録する変数
+float lastPressure;
+//CHECK_LANDED内のfor文用
+int i,j = 0;
+
 
 unsigned long time, previous_time;
 
@@ -73,15 +140,34 @@ unsigned long time, previous_time;
 void setup(void)
 {
 
+  //気圧
+  unsigned status;
+  status = bmp.begin(0x76); 
+  if (!status) { 
+//        digitalWrite(LEDR, LOW);
+        Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                      "try a different address!"));
+    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
+    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    Serial.print("        ID of 0x61 represents a BME 680.\n");
+           while (1) delay(10);
+
+  }
+  // 地表の気圧を読み取る
+  basePressure = bmp.readPressure();
+  lastPressure = basePressure;
+  
   //SD用
   sd_setup();
 
   //servo
   myservo.attach(6);
-  myservo.write(40);
-  delay(2000);
   myservo.write(180);
   delay(1000);
+  
+
 
 
   //モーター用
@@ -125,8 +211,149 @@ void setup(void)
 
 }
 
+void fall_detect(){
+    int count = -1;
+  double data[200][3];
+  while(1){
+    Serial.println(count);
+    count++;
+  
+  // if(count >= 10){
+  //   Serial.println("wrote in SD");
+  //   delay(1000);
+
+  //   myFile = SD.open("test.txt", FILE_WRITE);
+  //   for(int i = 0; i < 10; i++){
+
+  //     sd_write(data[i][0], data[i][1], data[i][2]);
+  //     delay(20);
+  //   }
+  //   myFile.close();
+  //   count = 0;
+
+  // }
+
+  
+  Serial.println("in Loop");
+  float pressure = bmp.readPressure();
+  float altitude = 44330 * ( 1 - pow(pressure/basePressure, 1/5.255) );
+
+  float x, y, z, accel;
+  imu::Vector<3> accelermetor = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  x = accelermetor.x();
+  y = accelermetor.y();
+  z = accelermetor.z();
+  accel = sqrt(x*x + y*y + z*z);
+  Serial.println(accel);
+
+  // turn all the LEDs off
+//  digitalWrite(LEDR, HIGH);
+//  digitalWrite(LEDG, HIGH);
+//  digitalWrite(LEDB, HIGH);
+  data[count][0] = altitude;
+  data[count][1] = accel;
+
+  // 最高高度を更新
+  if (altitude > highestAltitude) {
+    highestAltitude = altitude;
+    Serial.print("high");
+    Serial.println(altitude);
+  }
+  Serial.println(altitude);
+
+  
+
+  //ここからメインコード 
+  switch (state) {
+    case CHECK_FALLING:
+    data[count][2] = 0;
+    Serial.println("falling");
+
+
+
+//      digitalWrite(LEDB, LOW);
+      // 現在高度が最高地点からfallDiatance以上落下したとき、かつ加速度がfallAccelThreshold以下になったときに，落下を検出
+      if (abs(accel - fallAccelThreshold) > 0.5) {
+        fallStartTime = millis();
+        state = CHECK_LANDED;
+        lastPressure = basePressure;
+      }
+      break;
+      
+    case CHECK_LANDED:
+    data[count][2] = 1;
+
+          Serial.println("landed");
+
+//      digitalWrite(LEDR, LOW);
+      // 100*10ms=1sec*landedIdleDuration秒以上の間，加速度がほぼ1.0G（自由落下終了=重力加速度のみ)，かつ加速度変化がほぼない，かつ気圧の変化がほぼない場合、着地と判断。
+      // また，落下開始から一定時間経過した時は，無条件で着地と判断(強制分離)。
+      if (abs(fallAccelThreshold - accel) <= 0.2 && abs(accel - lastAccel) <= landedAccelThreshold && abs(pressure - lastPressure) <= landedPressureThreshold){
+        i++;
+        j=0;
+      }else if ((millis() - fallStartTime)/1000 >= ForcequitDuration) {
+        i = landedIdleDuration*10;
+        Serial.println(i);
+
+      }else{
+        j++;
+        if (j==2) i=0,j=0;
+      };
+      delay(100);
+      if (i <= 1) lastPressure = pressure; // 初回時の気圧を記憶
+      if (i >= landedIdleDuration*10) state = EXIT;
+      Serial.println("landed");
+      break;
+
+    case EXIT:
+    data[count][2] = 2;
+    Serial.println("wrote in SD");
+    myFile = SD.open("test.txt", FILE_WRITE);
+    for(int i = 0; i < count; i++){
+
+      sd_write(data[i][0], data[i][1], data[i][2]);
+      delay(20);
+    }
+    myFile.close();
+
+          Serial.println("done");
+          delay(1000);
+          return;
+          
+
+//      digitalWrite(LEDG, LOW);
+    while(1){
+
+    }
+      
+      
+      // サーボモーターを180度回転
+      state = COMPLETE;
+      Serial.println("complete");
+      ;
+
+    case COMPLETE:
+      break;
+
+    
+  }
+
+  // 前回の加速度を更新
+  lastAccel = accel;
+
+  delay(1000);
+  }
+}
+
 void loop(void)
 {
+
+
+  fall_detect();
+  myservo.write(40);
+  delay(2000);
+  myservo.write(180);
+  delay(1000);
 
 
   Serial.println("now in loop");
